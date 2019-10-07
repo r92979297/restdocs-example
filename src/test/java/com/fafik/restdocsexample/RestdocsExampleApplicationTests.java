5cccc11bba37526d97b4762358fb983d6689963a
r929297 <R92979297@gmail.com>
2019-10-07 21:10:39 +0200

    Initial commit

---
+++ src/test/java/com/fafik/restdocsexample/RestdocsExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fafik.restdocsexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestdocsExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
